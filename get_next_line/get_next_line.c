f2d2f38bafe196b138659814c4da02fe7915b728
jon <sanchezeguiajon@gmail.com>
2023-03-24 16:59:11 +0100

    WIP: Read concept

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -10,6 +10,29 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+//lo intuitivo sería devolver una linea
+//pero si opero sobre el pointer del str
+//puedo agilizar el proceso
+
+//leer v
+static int	ft_rdline(int fd, char **cash)
+{
+	char	*act;
+	char	*tot;
+	int		bytes;
+
+	test = malloc(sizeof(char) *(BUFFER_SIZE +1));
+	if (!test)
+		return (-1);
+	while () //line no tenga un \n o \0
+	{
+		bytes = read(fd, act, BUFFER_SIZE); //tengo que añadir \0? idk
+		if (bytes <= 0)
+			break;
+		tot = ft_strjoin(tot, act);  //igual enviar un int para saber si liberar memoria de los parametros enviados?
+	}	
+}
+
 char	*get_next_line(int fd)
 {
 	static char	*bank;
@@ -22,5 +45,8 @@ char	*get_next_line(int fd)
 		cash = bank; // igual se ahorra esta + (n+1) linea con un free en el join
 		free(bank);
 	}
+	else
+		//leer;
+	
 	return (line);
 }
