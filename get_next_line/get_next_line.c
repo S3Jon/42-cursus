ba5b746ac4718335dca93e62ce12688968368d35
jon <sanchezeguiajon@gmail.com>
2023-03-30 14:42:13 +0200

    Further read

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -10,27 +10,41 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "GET_NEXT_LINE.H"
 //lo intuitivo sería devolver una linea
 //pero si opero sobre el pointer del str
 //puedo agilizar el proceso
 
 //leer v
+
+static int	ft_gotline(char *c)
+{
+	while (*c != '\0')
+	{
+		if (*c == '\n')
+			return (1);
+		*c++;
+	}
+	return (0);
+}
 static int	ft_rdline(int fd, char **cash)
 {
 	char	*act;
 	char	*tot;
-	int		bytes;
+	int		rdbytes;
 
-	test = malloc(sizeof(char) *(BUFFER_SIZE +1));
-	if (!test)
+	act = malloc(sizeof(char) *(BUFFER_SIZE + 1));
+	if (!act)
 		return (-1);
-	while () //line no tenga un \n o \0
+	while (ft_gotline(tot) == 0) //line no tenga un \n o \0 ed2: igual añadir que lea mientras read != 0?
 	{
-		bytes = read(fd, act, BUFFER_SIZE); //tengo que añadir \0? idk
-		if (bytes <= 0)
+		rdbytes = read(fd, act, BUFFER_SIZE); //tengo que añadir \0? idk
+		if (rdbytes <= 0)
 			break;
 		tot = ft_strjoin(tot, act);  //igual enviar un int para saber si liberar memoria de los parametros enviados?
-	}	
+	}
+	free(act);
+	return (rdbytes);
 }
 
 char	*get_next_line(int fd)
@@ -41,7 +55,7 @@ char	*get_next_line(int fd)
 	if (bank)
 	{
 		if (ft_gotline(bank) == 1)
-			return (ft_reformat(&bank))
+			return (ft_reformat(&bank));
 		cash = bank; // igual se ahorra esta + (n+1) linea con un free en el join
 		free(bank);
 	}
