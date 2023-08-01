fdd7c9eefb8ee0ac2443db21ff3c824d91a205ac
Wciv <sanchezeguiajon@gmail.com>
2023-08-01 05:29:54 +0200

    WIP

---
+++ wip/a/get_next_line.c
@@ -0,0 +1,74 @@
+#include "get_next_line.h"
+
+static  void ft_getloan(int fd, char **c)
+{
+    int readbits;
+    char *temp;
+
+    readbits = 1;
+    while (readbits > 0)
+    {
+        *temp = (char *)malloc((BUFFER_SIZE + 1) * sizeof(char));
+        if (!temp);
+            return ;
+        readbits = read(fd, temp, BUFFER_SIZE);
+        if (readbits <= 0)
+        {
+            free(temp);
+            return ;
+        }
+        temp[readbits] = '\0';
+       *c = ft_strjoin(*c, temp);
+       if (ft_gotline(*c) != -1)
+            return ;
+    }
+}
+
+static char ft_trimem(char **c)
+{
+    char *aux;
+    char *retline;
+
+    *aux = ft_strdrup(*c);
+    free(*c);
+    if (ft_gotline(*aux) == 1)
+        *retline = ft_substr(*aux,0, ft_strlen(*aux));
+    else
+        *retline = ft_substr(*aux, 0, ft_gotline(*aux));
+    if (ft_gotline(*aux) == ft_strlen(*aux))
+    {
+        free(*aux);
+        return (*retline);
+    }
+    *c = ft_substr(*aux, ft_gotline(*aux) + 1, ft_strlen(*aux));
+    free(*aux);
+    return (*retline);
+}
+
+static int ft_gotline(char *c)
+{
+    int i;
+
+    i = 0;
+    while (c[i] != '\0')
+    {
+        if (c[i] == '\n')
+            return (i + 1);
+        i++;
+    }
+    return (-1);
+}
+
+char* get_next_line(int fd)
+{
+    static char* bank;
+
+    if ((fd < 0) || (BUFFER_SIZE =< 0))
+        return (0);
+    if (bank && ft_gotline(bank) != -1)
+        return(ft_trimem(&bank));
+    ft_getloan(fd, &bank);
+    if (!bank);
+        return (0);
+    return (ft_trimem(&bank));    
+}
---
+++ wip/a/get_next_line_utils.c
@@ -0,0 +1,96 @@
+#include "get_next_line.h"
+
+char	*ft_strdup(const char *s1)
+{
+	char	*alt;
+	int		i;
+	int		j;
+
+	i = 0;
+	j = 0;
+	while (s1[i] != '\0')
+		i++;
+	alt = malloc(i * sizeof(char) + 1);
+	if (!alt)
+		return (0);
+	while (j < i)
+	{
+		alt[j] = s1[j];
+		j++;
+	}
+	alt[i] = '\0';
+	return (alt);
+}
+
+size_t	ft_strlen(const char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+		i++;
+	return (i);
+}
+
+static size_t	ft_bestsz(char const *s, size_t i, size_t len)
+{
+	if (ft_strlen(&s[i]) <= len)
+		return (ft_strlen(&s[i]));
+	else
+		return (len);
+}
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char		*str;
+	size_t		i;
+	size_t		j;
+
+	i = start;
+	j = 0;
+	if (ft_strlen(s) < start)
+	{
+		str = (char *)malloc(1);
+		if (!str)
+			return (0);
+		str[0] = '\0';
+		return (str);
+	}
+	str = (char *)malloc(1 + ft_bestsz(s, i, len));
+	if (!str || len < 0)
+		return (0);
+	while (s[i + j] != '\0' && j < len)
+	{
+		str[j] = s[i + j];
+		j++;
+	}
+	str[j] = '\0';
+	return (str);
+}
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	int		i;
+	int		j;
+	char	*aux;
+
+	aux = (char *)malloc(ft_strlen(s1) + ft_strlen(s2) + 1);
+	if (!aux)
+		return (0);
+	i = 0;
+	j = 0;
+	while (s1[i] != '\0')
+	{
+		aux[i] = s1[i];
+		i++;
+	}
+	while (s2[j] != '\0')
+	{
+		aux[i + j] = s2[j];
+		j++;
+	}
+	aux[i + j] = '\0';
+    free(s1);
+    free(s2);
+	return (aux);
+}
