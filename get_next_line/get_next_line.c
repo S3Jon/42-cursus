b4f3ba21f4280bd4f1d4681799c5e213eab68d63
Jon Sanchez Eguia <jonsanch@c4r2s2.42urduliz.com>
2023-08-01 19:31:10 +0200

    SUCCESS: 100/100

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -5,62 +5,96 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: jonsanch <jonsanch@student.42urduliz.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/10/13 02:30:28 by jonsanch          #+#    #+#             */
-/*   Updated: 2022/10/13 02:30:28 by jonsanch         ###   ########.fr       */
+/*   Created: 2023/08/01 14:48:46 by jonsanch          #+#    #+#             */
+/*   Updated: 2023/08/01 15:03:39 by jonsanch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "GET_NEXT_LINE.H"
-//lo intuitivo sería devolver una linea
-//pero si opero sobre el pointer del str
-//puedo agilizar el proceso
+#include "get_next_line.h"
 
-//leer v
-
-static int	ft_gotline(char *c)
+static int	ft_isthisline(char *c)
 {
-	while (*c != '\0')
+	size_t	pos;
+
+	pos = 0;
+	while (c[pos] != '\0')
 	{
-		if (*c == '\n')
-			return (1);
-		*c++;
+		if (c[pos] == '\n')
+			return (pos);
+		pos++;
+	}
+	return (-1);
+}
+
+static int	ft_read_fd(int fd, char **c)
+{
+	int		read_bits;
+	char	*temp;
+
+	read_bits = 1;
+	while (read_bits > 0)
+	{
+		temp = (char *)malloc((BUFFER_SIZE + 1) * sizeof(char));
+		if (!temp)
+			return (0);
+		read_bits = read(fd, temp, BUFFER_SIZE);
+		if (read_bits == -1)
+		{
+			free(temp);
+			return (-1);
+		}
+		temp[read_bits] = '\0';
+		if (*c == NULL)
+			*c = ft_strdup(temp);
+		else
+			*c = ft_strjoin(*c, temp);
+		free(temp);
+		if (ft_isthisline(*c) != -1)
+			return (0);
 	}
 	return (0);
 }
-static int	ft_rdline(int fd, char **cash)
-{
-	char	*act;
-	char	*tot;
-	int		rdbytes;
 
-	act = malloc(sizeof(char) *(BUFFER_SIZE + 1));
-	if (!act)
-		return (-1);
-	while (ft_gotline(tot) == 0) //line no tenga un \n o \0 ed2: igual añadir que lea mientras read != 0?
-	{
-		rdbytes = read(fd, act, BUFFER_SIZE); //tengo que añadir \0? idk
-		if (rdbytes <= 0)
-			break;
-		tot = ft_strjoin(tot, act);  //igual enviar un int para saber si liberar memoria de los parametros enviados?
-	}
-	free(act);
-	return (rdbytes);
+static char	*ft_trimem(char **c)
+{
+	char	*aux;
+	char	*ret_line;
+	size_t	sum;
+
+	sum = 0;
+	aux = ft_strdup(*c);
+	free(*c);
+	*c = NULL;
+	if (!aux)
+		return (NULL);
+	if (ft_isthisline(aux) == -1)
+		return (aux);
+	sum = (size_t)ft_isthisline(aux) + 1;
+	ret_line = ft_substr(aux, 0, sum);
+	*c = ft_substr(aux, sum, (ft_strlen(aux) - sum));
+	free (aux);
+	return (ret_line);
 }
 
 char	*get_next_line(int fd)
 {
 	static char	*bank;
-	char		*cash;
 
-	if (bank)
+	if ((fd < 0) || (BUFFER_SIZE <= 0))
+		return (NULL);
+	if (bank && ft_isthisline(bank) != -1)
+		return (ft_trimem(&bank));
+	if (ft_read_fd(fd, &bank) == -1)
 	{
-		if (ft_gotline(bank) == 1)
-			return (ft_reformat(&bank));
-		cash = bank; // igual se ahorra esta + (n+1) linea con un free en el join
 		free(bank);
+		bank = NULL;
+		return (NULL);
 	}
-	else
-		//leer;
-	
-	return (line);
+	if (!bank || bank[0] == '\0')
+	{
+		free(bank);
+		bank = NULL;
+		return (NULL);
+	}
+	return (ft_trimem(&bank));
 }
--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -5,18 +5,24 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: jonsanch <jonsanch@student.42urduliz.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/03/23 04:16:52 by jonsanch          #+#    #+#             */
-/*   Updated: 2023/03/23 04:16:52 by jonsanch         ###   ########.fr       */
+/*   Created: 2023/08/01 14:48:24 by jonsanch          #+#    #+#             */
+/*   Updated: 2023/08/01 15:08:32 by jonsanch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
+# include <stdlib.h>
+# include <unistd.h>
 
-# ifdenf BUFFER_SIZE
-#  define BUFFER_SIZE 107
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 1024
 # endif
 
+char	*ft_strdup(const char *s1);
+size_t	ft_strlen(const char *str);
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_substr(char const *s, size_t start, size_t len);
 char	*get_next_line(int fd);
 
 #endif
