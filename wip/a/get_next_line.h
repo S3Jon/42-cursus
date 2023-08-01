1a8c7c4b8042ec60ea23d2114ad80c90c085169a
Wciv <sanchezeguiajon@gmail.com>
2023-08-01 12:44:01 +0200

    header norm1

--- wip/a/get_next_line.h
+++ wip/a/get_next_line.h
@@ -16,13 +16,13 @@
 # include <unistd.h>
 
 # ifndef BUFFER_SIZE
-#  define BUFFER_SIZE 1024;
+#	define BUFFER_SIZE 1024;
 # endif
 
-char    *ft_strdup(const char *s1);
-size_t ft_strlen(const char *str);
-char    *ft_strjoin(char const *s1, char const *s2);
-char    *ft_substr(char const *s, size_t start, size_t len);
-char *get_next_line(int fd);
+char	*ft_strdup(const char *s1);
+size_t	ft_strlen(const char *str);
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_substr(char const *s, size_t start, size_t len);
+char	*get_next_line(int fd);
 
 #endif
