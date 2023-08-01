7d15d181bdacbd8885ee2a374cbb0b986e21635a
Wciv <sanchezeguiajon@gmail.com>
2023-08-01 10:14:19 +0200

    Wip2.32

--- wip/a/get_next_line.c
+++ wip/a/get_next_line.c
@@ -54,7 +54,7 @@ static char *ft_trimem(char **c)
         return (NULL);
     free(*c);
     if (ft_isthisline(aux) == -1)
-        return (retline);
+        return (aux);
     retline = ft_substr(aux, 0, ft_isthisline(aux) + 1);
     *c = ft_substr(aux, ft_isthisline(aux) + 1, ft_strlen(aux));
     free (aux);
