719fda346520b01c3226253226900c0ea344c8fc
Wciv <sanchezeguiajon@gmail.com>
2023-08-01 10:49:52 +0200

    Wip2.38-roll

--- wip/a/get_next_line.c
+++ wip/a/get_next_line.c
@@ -50,14 +50,14 @@ static char *ft_trimem(char **c)
     char *retline;
 
     aux = ft_strdup(*c);
-    free(*c);
     if (!aux)
         return (NULL);
+    free(*c);
     if (ft_isthisline(aux) == -1)
         return (aux);
     retline = ft_substr(aux, 0, ft_isthisline(aux) + 1);
     if ((size_t)ft_isthisline(retline) + 1 != ft_strlen(aux))
-        *c = ft_substr(aux, (size_t)ft_isthisline(aux) + 1, ft_strlen(aux));
+        *c = ft_substr(aux, ft_isthisline(aux) + 1, ft_strlen(aux));
     free (aux);
     return (retline);
 }
